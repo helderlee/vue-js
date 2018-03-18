91b1bebaeb1cf88295ad1b2fd3419989a0b1e0e2
Helder <helderlee@yahoo.com>
2018-03-18 09:16:49 -0300

    Solution

--- exercise-02/app.js
+++ exercise-02/app.js
@@ -1,6 +1,14 @@
 new Vue({
-        el: '#exercise',
-        data: {
-            value: ''
+    el: '#exercise',
+    data: {
+        value: ''
+    },
+    methods: {
+        showAlert: function () {
+            alert("Button clicked!");
+        },
+        onKeydown: function (event) {
+            this.value = event.target.value;
         }
-    });
+    }
+});
