f39653af9af381da9ac26f39f80b56c08a773496
Helder <helderlee@yahoo.com>
2018-03-18 15:07:58 -0300

    Exercise 3

---
+++ exercise-03/app.js
@@ -0,0 +1,21 @@
+new Vue({
+    el: '#exercise',
+    data: {
+        value: 0
+    },
+    computed: {
+        result: function () {
+            return this.value == 37 ? 'Done' : 'Not there yet';
+        }
+    },
+    watch: {
+        result: function (value) {
+            var vm = this;
+            if (value == 'Done') {
+                setTimeout(function () {
+                    vm.value = 0;
+                }, 5000);
+            }
+        }
+    }
+});
